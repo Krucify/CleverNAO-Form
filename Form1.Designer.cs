b0e44b1a630119d85205fe6dce46922bb44e5b52
Krucify <jrserran@eagle.fgcu.edu>
2015-04-21 09:56:57 -0400

    Initial Commit
    All changes/completed project here.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CleverNAO_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.convoBox = new System.Windows.Forms.RichTextBox();
+            this.clearConvoBtn = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.speechOnOff = new System.Windows.Forms.Button();
+            this.ErrorLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // convoBox
+            // 
+            this.convoBox.Location = new System.Drawing.Point(12, 12);
+            this.convoBox.Name = "convoBox";
+            this.convoBox.Size = new System.Drawing.Size(463, 219);
+            this.convoBox.TabIndex = 1;
+            this.convoBox.Text = "";
+            this.convoBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pushToTalk_keyDown);
+            this.convoBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.pushToTalk_keyUp);
+            // 
+            // clearConvoBtn
+            // 
+            this.clearConvoBtn.Location = new System.Drawing.Point(339, 294);
+            this.clearConvoBtn.Name = "clearConvoBtn";
+            this.clearConvoBtn.Size = new System.Drawing.Size(84, 36);
+            this.clearConvoBtn.TabIndex = 2;
+            this.clearConvoBtn.Text = "Clear Conversation";
+            this.clearConvoBtn.UseVisualStyleBackColor = true;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(12, 256);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(463, 20);
+            this.inputBox.TabIndex = 3;
+            this.inputBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pushToTalk_keyDown);
+            this.inputBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputBox_enter);
+            this.inputBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.pushToTalk_keyUp);
+            // 
+            // speechOnOff
+            // 
+            this.speechOnOff.BackColor = System.Drawing.SystemColors.Control;
+            this.speechOnOff.Location = new System.Drawing.Point(62, 294);
+            this.speechOnOff.Name = "speechOnOff";
+            this.speechOnOff.Size = new System.Drawing.Size(88, 36);
+            this.speechOnOff.TabIndex = 4;
+            this.speechOnOff.Text = "Off";
+            this.speechOnOff.UseVisualStyleBackColor = false;
+            // 
+            // ErrorLabel
+            // 
+            this.ErrorLabel.AutoSize = true;
+            this.ErrorLabel.ForeColor = System.Drawing.Color.Red;
+            this.ErrorLabel.Location = new System.Drawing.Point(217, 349);
+            this.ErrorLabel.Name = "ErrorLabel";
+            this.ErrorLabel.Size = new System.Drawing.Size(0, 13);
+            this.ErrorLabel.TabIndex = 5;
+            this.ErrorLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 371);
+            this.Controls.Add(this.ErrorLabel);
+            this.Controls.Add(this.speechOnOff);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.clearConvoBtn);
+            this.Controls.Add(this.convoBox);
+            this.Name = "Form1";
+            this.Text = "CleverNAO";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pushToTalk_keyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.pushToTalk_keyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox convoBox;
+        private System.Windows.Forms.Button clearConvoBtn;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button speechOnOff;
+        private System.Windows.Forms.Label ErrorLabel;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Speech.Recognition;
+using Aldebaran.Proxies;
+using ChatterBotAPI;
+
+namespace CleverNAO_Form
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine sre;
+        private bool recognitionRunning;
+        private bool connectedToNAO;
+        private bool connectedToCleverBot;
+
+        ChatterBotFactory factory;
+        ChatterBot robot;
+        ChatterBotSession session;
+
+        TextToSpeechProxy tts;
+
+        BackgroundWorker bw;
+
+        public Form1()
+        {
+            InitializeComponent();
+            speechOnOff.Enabled = false;
+            ErrorLabel.Text = "";
+            clearConvoBtn.Click += new System.EventHandler(clearConvoBtn_click);
+            initializeConvoBox();
+            initializeBackgroundWorker();
+            startSpeech();
+            startCleverbot();
+            startNAO();
+        }
+
+        #region Initialization Functions
+        public void initializeConvoBox()
+        {
+            //string start = "Hold Shift to speak, then release.";
+            convoBox.ReadOnly = true;
+            convoBox.BackColor = System.Drawing.SystemColors.Window;
+            //insertConvoLine(start, true);
+        }
+
+        public void initializeBackgroundWorker()
+        {
+            bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+        }
+        public void startSpeech()
+        {
+            System.Speech.Recognition.DictationGrammar dg = new System.Speech.Recognition.DictationGrammar();
+            dg.Enabled = true;
+            
+            sre = new SpeechRecognitionEngine();
+            sre.LoadGrammar(dg);
+
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+            sre.SetInputToDefaultAudioDevice();
+        }
+
+        public void startCleverbot()
+        {
+            factory = new ChatterBotFactory();
+            robot = factory.Create(ChatterBotType.CLEVERBOT);
+            session = robot.CreateSession();
+
+        }
+
+        public void startNAO()
+        {
+            try
+            {
+                tts = new TextToSpeechProxy("192.168.0.102", 9559);
+                connectedToNAO = true;
+            }
+            catch (Exception e)
+            {
+                connectedToNAO = false;
+                ErrorLabel.Text = "No Connection to NAO";
+            }
+        }
+        #endregion
+        #region Helper Functions
+        public void outputToConvoBox(string line, int type)
+        {
+            convoBox.AppendText("(" + DateTime.Now.ToLongTimeString() + ") ");
+            int length = convoBox.TextLength;
+            switch (type)
+            {
+                case 1: // From robot
+                    convoBox.AppendText("Robot: " + line + "\n");
+                    convoBox.SelectionStart = length;
+                    convoBox.SelectionLength = line.Length + 8;
+                    convoBox.SelectionColor = Color.Blue;
+                    if (connectedToNAO)
+                        sendToNAO(line);
+                    break;
+                case 2: // From human
+                    convoBox.AppendText("Human: " + line + "\n");
+                    convoBox.SelectionStart = length;
+                    convoBox.SelectionLength = line.Length + 8;
+                    convoBox.SelectionColor = Color.Black;
+                    break;
+                case 3: // System messages
+                    convoBox.AppendText(line + "\n");
+                    convoBox.SelectionStart = length;
+                    convoBox.SelectionLength = line.Length + 1;
+                    convoBox.SelectionColor = Color.Red;
+                    break;
+                default:
+                    break;
+            }
+            inputBox.Enabled = true;
+            inputBox.Focus();
+            convoBox.SelectionStart = convoBox.Text.Length;
+            convoBox.ScrollToCaret();
+        }
+
+        public string getResponse(string input)
+        {
+            try
+            {
+                connectedToCleverBot = true;
+                return session.Think(input);
+            }
+            catch (Exception e)
+            {
+                connectedToCleverBot = false;
+                return "Error: " + e.Message;
+            }
+        }
+
+        public void sendToNAO(string input)
+        {
+            tts.say(input);
+        }
+#endregion
+        #region EVENT HANDLERS
+        void pushToTalk_keyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey && !recognitionRunning)
+            {
+                try
+                {
+                    sre.RecognizeAsync();
+                    speechOnOff.BackColor = Color.LimeGreen;
+                    speechOnOff.Text = "On";
+                    e.Handled = true;
+                    e.SuppressKeyPress = true;
+                    recognitionRunning = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        void pushToTalk_keyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                sre.RecognizeAsyncCancel();
+                speechOnOff.BackColor = System.Drawing.SystemColors.Control;
+                speechOnOff.Text = "Off";
+                recognitionRunning = false;
+            }
+        }
+
+        void inputBox_enter(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13) 
+            {
+                if (!bw.IsBusy)
+                {
+                    outputToConvoBox(inputBox.Text, 2);
+                    bw.RunWorkerAsync(inputBox.Text);
+                    inputBox.Clear();
+                    inputBox.Enabled = false;
+                }
+            }
+                
+        }
+        void clearConvoBtn_click(object sender, EventArgs e)
+        {
+            convoBox.Clear();
+        }
+
+        // Speech Event Handlers
+        void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (!bw.IsBusy && e.Result.Confidence > 0.3)
+            {
+                outputToConvoBox(e.Result.Text, 2);
+                bw.RunWorkerAsync(e.Result.Text);
+            }
+            else
+            {
+                outputToConvoBox("Try Again. Speak Clearly.", 3);
+            }
+        }
+
+        void recognizer_UnrecognizedSpeech(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            if (!bw.IsBusy)
+            {
+                outputToConvoBox("Speech not recognized. " + e.Result.Text, 3);
+            }
+        }
+
+        //Background Worker Event Handlers
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            e.Result = getResponse((string)e.Argument);
+        }
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!connectedToCleverBot)
+                outputToConvoBox((string)e.Result, 3);
+            else
+                outputToConvoBox((string)e.Result, 1);
+        }
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+        }
+
+#endregion
+    }
+}
